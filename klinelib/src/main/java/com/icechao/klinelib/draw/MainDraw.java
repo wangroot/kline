b73dabc8121e94f2d838f356c1c262525f52cf6f
chao <icechliu@gmail.com>
2019-03-20 10:49:34 +0800

    MainDraw.java
    优化部分手机卡顿的问题

--- klinelib/src/main/java/com/icechao/klinelib/draw/MainDraw.java
+++ klinelib/src/main/java/com/icechao/klinelib/draw/MainDraw.java
@@ -1,11 +1,9 @@
 package com.icechao.klinelib.draw;
 
-import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.RectF;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -15,24 +13,28 @@ import com.icechao.klinelib.base.IChartDraw;
 import com.icechao.klinelib.base.IValueFormatter;
 import com.icechao.klinelib.entity.ICandle;
 import com.icechao.klinelib.formatter.ValueFormatter;
+import com.icechao.klinelib.utils.NumberUtil;
 import com.icechao.klinelib.utils.ViewUtil;
+
+import java.util.Arrays;
 
 /*************************************************************************
  * Description   :
  *
- * @PackageName  : com.icechao.klinelib.utils
+ * @PackageName  : com.huobi.klinelib.utils
  * @FileName     : MainDraw.java
  * @Author       : chao
  * @Date         : 2019/1/8
  * @Email        : icechliu@gmail.com
  * @version      : V1
  *************************************************************************/
-public class MainDraw implements IChartDraw<ICandle> {
+public class MainDraw<T extends ICandle> implements IChartDraw<T> {
 
     private int itemCount;
     private final float padding;
     private final float margin;
     private String[] strings = new String[8];
+    private ValueFormatter valueFormatter = new ValueFormatter();
 
     public void setItemCount(int mItemCount) {
         itemCount = mItemCount;
@@ -81,7 +83,9 @@ public class MainDraw implements IChartDraw<ICandle> {
     @Override
     @SuppressWarnings("all")
     public void drawTranslated(@Nullable ICandle lastPoint, @NonNull ICandle curPoint, float lastX, float curX, @NonNull Canvas canvas, @NonNull BaseKLineChartView view, int position) {
-
+        if (null == curPoint) {
+            return;
+        }
         if (view.isLine()) {
             if (position == itemCount - 1) {
                 float lastClosePrice = lastPoint.getClosePrice();
@@ -100,52 +104,26 @@ public class MainDraw implements IChartDraw<ICandle> {
             Status status = view.getStatus();
             if (status == Status.MA) {
                 //画第一根ma
-                if (Float.MIN_VALUE != lastPoint.getMaOne()) {
-                    if (itemCount - 1 == position && 0 != this.maOne) {
-                        view.drawLine(canvas, indexPaintOne, lastX, lastPoint.getMaOne(), curX, this.maOne);
-                    } else {
-                        view.drawLine(canvas, indexPaintOne, lastX, lastPoint.getMaOne(), curX, curPoint.getMaOne());
-                    }
-                }
+                drawLine(lastX, curX, canvas, view, position, lastPoint.getMaOne(), maOne, indexPaintOne, curPoint.getMaOne());
                 //画第二根ma
-                if (Float.MIN_VALUE != lastPoint.getMaTwo()) {
-                    if (itemCount - 1 == position && 0 != this.maTwo) {
-                        view.drawLine(canvas, indexPaintTwo, lastX, lastPoint.getMaTwo(), curX, this.maTwo);
-                    } else {
-                        view.drawLine(canvas, indexPaintTwo, lastX, lastPoint.getMaTwo(), curX, curPoint.getMaTwo());
-                    }
-                }
+                drawLine(lastX, curX, canvas, view, position, lastPoint.getMaTwo(), maTwo, indexPaintTwo, curPoint.getMaTwo());
                 //画第三根ma
-                if (Float.MIN_VALUE != lastPoint.getMaThree()) {
-                    if (itemCount - 1 == position && 0 != this.maThree) {
-                        view.drawLine(canvas, indexPaintThree, lastX, lastPoint.getMaThree(), curX, this.maThree);
-                    } else {
-                        view.drawLine(canvas, indexPaintThree, lastX, lastPoint.getMaThree(), curX, curPoint.getMaThree());
-                    }
-                }
+                drawLine(lastX, curX, canvas, view, position, lastPoint.getMaThree(), maThree, indexPaintThree, curPoint.getMaThree());
             } else if (status == Status.BOLL) {
                 //画boll
-                if (Float.MIN_VALUE != lastPoint.getUp()) {
-                    if (itemCount - 1 == position && 0 != bollUp) {
-                        view.drawLine(canvas, indexPaintOne, lastX, lastPoint.getUp(), curX, bollUp);
-                    } else {
-                        view.drawLine(canvas, indexPaintOne, lastX, lastPoint.getUp(), curX, curPoint.getUp());
-                    }
-                }
-                if (Float.MIN_VALUE != lastPoint.getMb()) {
-                    if (itemCount - 1 == position && 0 != bollMb) {
-                        view.drawLine(canvas, indexPaintTwo, lastX, lastPoint.getMb(), curX, bollMb);
-                    } else {
-                        view.drawLine(canvas, indexPaintTwo, lastX, lastPoint.getMb(), curX, curPoint.getMb());
-                    }
-                }
-                if (Float.MIN_VALUE != lastPoint.getDn()) {
-                    if (itemCount - 1 == position && 0 != bollDn) {
-                        view.drawLine(canvas, indexPaintThree, lastX, lastPoint.getDn(), curX, bollDn);
-                    } else {
-                        view.drawLine(canvas, indexPaintThree, lastX, lastPoint.getDn(), curX, curPoint.getDn());
-                    }
-                }
+                drawLine(lastX, curX, canvas, view, position, lastPoint.getUp(), bollUp, indexPaintTwo, curPoint.getUp());
+                drawLine(lastX, curX, canvas, view, position, lastPoint.getMb(), bollMb, indexPaintOne, curPoint.getMb());
+                drawLine(lastX, curX, canvas, view, position, lastPoint.getDn(), bollDn, indexPaintThree, curPoint.getDn());
+            }
+        }
+    }
+
+    private void drawLine(float lastX, float curX, @NonNull Canvas canvas, @NonNull BaseKLineChartView view, int position, float start, float animEnd, Paint paint, float end) {
+        if (Float.MIN_VALUE != start) {
+            if (itemCount - 1 == position && 0 != animEnd && view.isAnimationLast()) {
+                view.drawLine(canvas, paint, lastX, start, curX, animEnd);
+            } else {
+                view.drawLine(canvas, paint, lastX, start, curX, end);
             }
         }
     }
@@ -174,27 +152,27 @@ public class MainDraw implements IChartDraw<ICandle> {
             ICandle point = view.getItem(position);
             if (status == Status.MA) {
                 String text;
-                if (0 != point.getMaOne()) {
-                    text = "MA5:" + view.formatValue(point.getMaOne()) + "  ";
+                if (Float.MIN_VALUE != point.getMaOne()) {
+                    text = "MA5:" + getValueFormatter().format(point.getMaOne()) + "  ";
                     canvas.drawText(text, x, y, indexPaintOne);
                     x += indexPaintOne.measureText(text);
                 }
-                if (0 != point.getMaTwo()) {
-                    text = "MA10:" + view.formatValue(point.getMaTwo()) + "  ";
+                if (Float.MIN_VALUE != point.getMaTwo()) {
+                    text = "MA10:" + getValueFormatter().format(point.getMaTwo()) + "  ";
                     canvas.drawText(text, x, y, indexPaintTwo);
                     x += indexPaintTwo.measureText(text);
                 }
-                if (0 != point.getMaThree()) {
-                    text = "MA30:" + view.formatValue(point.getBollMa());
+                if (Float.MIN_VALUE != point.getMaThree()) {
+                    text = "MA30:" + getValueFormatter().format(point.getMaThree());
                     canvas.drawText(text, x, y, indexPaintThree);
                 }
             } else if (status == Status.BOLL) {
                 if (0 != point.getMb()) {
                     String text = "BOLL:" + view.formatValue(point.getMb()) + "  ";
-                    canvas.drawText(text, x, y, indexPaintTwo);
+                    canvas.drawText(text, x, y, indexPaintOne);
                     x += indexPaintOne.measureText(text);
                     text = "UB:" + view.formatValue(point.getUp()) + "  ";
-                    canvas.drawText(text, x, y, indexPaintOne);
+                    canvas.drawText(text, x, y, indexPaintTwo);
                     x += indexPaintTwo.measureText(text);
                     text = "LB:" + view.formatValue(point.getDn());
                     canvas.drawText(text, x, y, indexPaintThree);
@@ -209,41 +187,46 @@ public class MainDraw implements IChartDraw<ICandle> {
     @Override
     public float getMaxValue(ICandle point, Status status) {
         if (status != Status.BOLL) {
-            return Math.max(point.getHighPrice(), point.getBollMa());
+            float max = Math.max(point.getHighPrice(), point.getMaOne());
+            max = Math.max(max, Math.max(point.getMaTwo(), point.getMaThree()));
+            return max;
         }
-        if (Float.isNaN(point.getUp())) {
-            if (point.getMb() == 0) {
-                return point.getHighPrice();
-            } else {
-                return point.getMb();
-            }
-        } else if (point.getUp() == 0) {
+        if (point.getUp() != Float.MIN_VALUE) {
             return point.getHighPrice();
         } else {
-            return point.getUp();
+            return Math.max(point.getHighPrice(), point.getUp());
         }
     }
 
     @Override
     public float getMinValue(ICandle point, Status status) {
         if (status == Status.BOLL) {
-            if (point.getDn() == 0) {
+            if (point.getDn() == Float.MIN_VALUE) {
                 return point.getLowPrice();
             } else {
                 return point.getDn();
             }
         } else {
-            if (point.getBollMa() == 0f) {
-                return point.getLowPrice();
-            } else {
-                return Math.min(point.getBollMa(), point.getLowPrice());
+            float[] floats = {point.getMaOne(), point.getMaTwo(), point.getMaThree(), point.getDn(), point.getLowPrice()};
+            Arrays.sort(floats);
+            for (int i = 0; i < floats.length; i++) {
+                if (floats[i] != Float.MIN_VALUE) {
+                    return floats[i];
+                }
             }
+            return point.getLowPrice();
         }
     }
 
     @Override
     public IValueFormatter getValueFormatter() {
-        return new ValueFormatter();
+
+        return valueFormatter;
+    }
+
+    @Override
+    public void setValueFormatter(IValueFormatter valueFormatter) {
+        this.valueFormatter = new ValueFormatter();
     }
 
     /**
@@ -268,29 +251,22 @@ public class MainDraw implements IChartDraw<ICandle> {
         float r = candleWidth / 2 * view.getScaleX();
         float cancleLeft = x - r;
         float candleright = x + r;
-        Path path = new Path();
-
         if (open > close) {
-            path.moveTo(x, high);
-            path.lineTo(x, open);
-            path.moveTo(x, close);
-            path.lineTo(x, low);
-            canvas.drawRect(cancleLeft, close, candleright, open, upPaint);
-            canvas.drawPath(path, upLinePaint);
-        } else if (open < close) {
-            path.moveTo(x, high);
-            path.lineTo(x, close);
-            path.moveTo(x, open);
-            path.lineTo(x, low);
-            canvas.drawRect(cancleLeft, open, candleright, close, downPaint);
-            canvas.drawPath(path, downLinePaint);
+            drawCandle(canvas, x, high, low, close, open, cancleLeft, candleright, downPaint, downLinePaint);
         } else {
-
-            path.moveTo(x, high);
-            path.lineTo(x, low);
-            canvas.drawRect(cancleLeft, open, candleright, close + 1, upPaint);
-            canvas.drawPath(path, upLinePaint);
+            drawCandle(canvas, x, high, low, open, close + 1, cancleLeft, candleright, upPaint, upLinePaint);
         }
+    }
+
+    private void drawCandle(Canvas canvas, float x, float high, float low, float open, float close, float cancleLeft, float candleright, Paint upPaint, Paint upLinePaint) {
+//        path.moveTo(x, high);
+//        path.lineTo(x, open);
+//        path.moveTo(x, close);
+//        path.lineTo(x, low);
+        canvas.drawRect(cancleLeft, close, candleright, open, upPaint);
+//        canvas.drawLine(x,high,x,open,x,close,x,low, upLinePaint);
+        canvas.drawLine(x, high, x, open, upPaint);
+        canvas.drawLine(x, close, x, low, upPaint);
     }
 
     /**
@@ -305,15 +281,15 @@ public class MainDraw implements IChartDraw<ICandle> {
         int index = view.getSelectedIndex();
 
         ICandle point = view.getItem(index);
-        strings[0] = String.valueOf(view.getAdapter().getDate(index));
-        strings[1] = (String.valueOf(point.getOpenPrice()));
-        strings[2] = (String.valueOf(point.getHighPrice()));
-        strings[3] = (String.valueOf(point.getLowPrice()));
-        strings[4] = (String.valueOf(point.getClosePrice()));
+        strings[0] = view.formatDateTime(view.getAdapter().getDate(index));
+        strings[1] = view.getValueFormatter().format(point.getOpenPrice());
+        strings[2] = (view.getValueFormatter().format(point.getHighPrice()));
+        strings[3] = (view.getValueFormatter().format(point.getLowPrice()));
+        strings[4] = (view.getValueFormatter().format(point.getClosePrice()));
         float tempDiffPrice = point.getClosePrice() - point.getOpenPrice();
-        strings[5] = (String.valueOf(tempDiffPrice));
-        strings[6] = (String.format("%.2f", (tempDiffPrice) * 100 / point.getOpenPrice()) + "%");
-        strings[7] = (String.valueOf(point.getVolume()));
+        strings[5] = (view.getValueFormatter().format(tempDiffPrice));
+        strings[6] = NumberUtil.roundFormatDown((tempDiffPrice * 100) / point.getOpenPrice(), 2) + "%";
+        strings[7] = NumberUtil.getTradeMarketAmount(valueFormatter.format(point.getVolume()));
 
         float width = 0, left, top = margin + view.getTopPadding();
         //上下多加两个padding值的间隙
@@ -334,8 +310,8 @@ public class MainDraw implements IChartDraw<ICandle> {
 
         float right = left + width;
         RectF r = new RectF(left, top, right, top + height);
-        canvas.drawRoundRect(r, padding, padding, selectorBackgroundPaint);
-        canvas.drawRoundRect(r, padding, padding, selectorBorderPaint);
+        canvas.drawRoundRect(r, padding / 2, padding / 2, selectorBackgroundPaint);
+        canvas.drawRoundRect(r, padding / 2, padding / 2, selectorBorderPaint);
         float y = top + padding * 2 + selectedTextBaseLine;
         float tempX = right - padding;
         for (int i = 0; i < length; i++) {
@@ -477,54 +453,25 @@ public class MainDraw implements IChartDraw<ICandle> {
             return;
         }
         if (status == Status.MA) {
-            view.generaterAnimator(maOne, item.getMaOne(), new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    maOne = (float) animation.getAnimatedValue();
-                }
-            });
-            view.generaterAnimator(maTwo, item.getMaTwo(), new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    maTwo = (float) animation.getAnimatedValue();
-                }
-            });
-            view.generaterAnimator(maThree, item.getMaThree(), new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    maThree = (float) animation.getAnimatedValue();
-                }
-            });
+            view.generaterAnimator(maOne, item.getMaOne(), animation -> maOne = (float) animation.getAnimatedValue());
+            view.generaterAnimator(maTwo, item.getMaTwo(), animation -> maTwo = (float) animation.getAnimatedValue());
+            view.generaterAnimator(maThree, item.getMaThree(), animation -> maThree = (float) animation.getAnimatedValue());
         } else if (status == Status.BOLL) {
-            view.generaterAnimator(bollMb, item.getMb(), new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    bollMb = (float) animation.getAnimatedValue();
-                }
-            });
-            view.generaterAnimator(bollDn, item.getDn(), new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    bollDn = (float) animation.getAnimatedValue();
-                }
-            });
-            view.generaterAnimator(bollUp, item.getUp(), new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    bollUp = (float) animation.getAnimatedValue();
-                }
-            });
+            view.generaterAnimator(bollMb, item.getMb(), animation -> bollMb = (float) animation.getAnimatedValue());
+            view.generaterAnimator(bollDn, item.getDn(), animation -> bollDn = (float) animation.getAnimatedValue());
+            view.generaterAnimator(bollUp, item.getUp(), animation -> bollUp = (float) animation.getAnimatedValue());
         }
     }
 
     @Override
-    public void setValueFormatter(IValueFormatter valueFormatter) {
-
-    }
-
-    @Override
     public void resetValues() {
+        maOne = 0;
+        maTwo = 0;
+        maThree = 0;
 
+        bollUp = 0;
+        bollMb = 0;
+        bollDn = 0;
     }
 
     public void setMarketInfoText(String[] marketInfoText) {
